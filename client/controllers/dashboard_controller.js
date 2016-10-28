bcc7600ba8e90d6bdd83f229147309944fc24142
aminhp93 <minhpn.org.ec@gmail.com>
2016-10-27 19:32:46 -0700

    final one

--- client/controllers/dashboard_controller.js
+++ client/controllers/dashboard_controller.js
@@ -13,11 +13,13 @@ app.controller('dashboardController', function($scope, dashboardFactory, $locati
                 for (var i = 0; i < $scope.users.length; i++) {
                     console.log($scope.users);
                     console.log($scope.users[i]);
-
+                    console.log($cookies.get('user_id'));
+                    console.log($scope.users[i].facebook_id);
+                    $scope.result = [];
                     if ($cookies.get('user_id') != $scope.users[i].facebook_id) {
                         console.log('reusltsjfjads;')
                         var distance = calculateDistance($scope.users[i].lat, $scope.users[i].lon, pos.lat, pos.lon);
-                        $scope.result = [];
+
                         if (distance < 1) {
                             console.log('testing')
                             distance = distance.toFixed(3);
